dd5371d40e91cb9c7425d707030ca69a6d0e611d
Flash <HeyFlash@gmx.net>
2015-01-10 22:54:25 +0100

    Added automatic enum translation.

--- cpp-to-clbind.el
+++ cpp-to-clbind.el
@@ -5,16 +5,23 @@
 ;;; Edit the keymap at the end of the file, evaluate the file
 ;;; and activate cpp2clbind-mode.
 
-(defvar cpp2clb/current-namespace "sf"
+(defconst cpp2clb/current-namespace "sf"
   "Always used as the current namespace.")
 
-(defvar cpp2clb/current-class nil
+(defconst cpp2clb/current-class nil
   "Used as the name of the current class.
 If nil, the name of the class is tried to be autodetected.")
 
-(defvar cpp2clb/static-class-delimiter "/"
+(defconst cpp2clb/current-package "SFML"
+  "Used as the CL package in which to intern things.")
+
+(defconst cpp2clb/static-class-delimiter "/"
   "The delimiter between class name and function name,
 used when static member functions are translated.")
+
+(defconst cpp2clb/enum-class-delimiter "/"
+  "The delimiter between class name and enum name, 
+used when enums are translated to symbols.")
 
 (defconst cpp2clb/string-literal-prefix "R"
   "The prefix for the string literal of the docstring.")
@@ -27,7 +34,7 @@ used when static member functions are translated.")
 
 (defconst cpp2clb/no-comment-regexp "^[^/]*$"
   "The regexp that is used to distinguish lines that are
-not comments")
+not comments.")
 
 ;; (defconst cpp2clb/remove-line-from-comment-regexp "^\\s-*////+$"
 ;;   "The regexp that is used to remove entire lines from comments.")
@@ -36,10 +43,13 @@ not comments")
   "The regexp that is used to delete matches from comments.")
 
 (defconst cpp2clb/function-name-regexp "\s+\\sw+("
-  "The regexp that is used to detect the name of a function")
+  "The regexp that is used to detect the name of a function.")
 
 (defconst cpp2clb/class-regexp "\s*class\s+"
-  "The regexp that is used to find the current class")
+  "The regexp that is used to find the current class.")
+
+(defconst cpp2clb/enum-regexp "\s*enum\s+"
+  "The regexp that is used to find the current enum.")
 
 (defconst cpp2clb/api-def-regexp
   "\\(SFML_SYSTEM_API\\|SFML_WINDOW_API\\|SFML_GRAPHICS_API\\|SFML_AUDIO_API\\|SFML_NETWORK_API\\)"
@@ -63,7 +73,7 @@ second-regexp."
     (re-search-forward second-regexp)
     (buffer-substring-no-properties (mark) (point))))
 
-(defun cpp2clb/get-function-string-at-point ()
+(defun cpp2clb/get-complete-definition-at-point ()
   "Returns the whole string representing an SFML function,            
 including the doxygen documentation"
   (cpp2clb/get-text-between-regexps "^$" "^$"))
@@ -82,17 +92,38 @@ i.e. where the comment ends."
   "Returns the function definition at the current point,
 even if point is inside the doxy string"
   (cpp2clb/get-function-definition
-   (cpp2clb/get-function-string-at-point)))
+   (cpp2clb/get-complete-definition-at-point)))
+
+(defun cpp2clb/get-brace-content (string open-brace close-brace)
+  "Get the content between them matches of open-brace and close-brace
+in string."
+  (substring string
+	     (+ (string-match open-brace string) (length open-brace))
+	     (string-match close-brace string)))
+
+(defun cpp2clb/get-lines-as-list (string)
+  "Get the lines of the string as a list"
+  (split-string (cpp2clb/chomp string) "$" t))
+
+(defun cpp2clb/get-enum-member-from-line (line)
+  "Get the enum member from a single line, if it is at start of line."
+  (car (split-string (cpp2clb/chomp line)
+		     "[,\|\s-]")))
 
 (defun cpp2clb/get-doxystring (function-with-doxy)
   "Extracts the doxystring from function-with-doxy"
   (substring function-with-doxy
-   0 (cpp2clb/get-function-definition-position function-with-doxy)))
+	     0 (cpp2clb/get-function-definition-position function-with-doxy)))
+
+(defun cpp2clb/get-non-doxystring (thing-with-doxy)
+  "Extracts everything beyond the doxystring from thing-with-doxy"
+  (substring thing-with-doxy
+	     (cpp2clb/get-function-definition-position thing-with-doxy)))
 
 (defun cpp2clb/get-doxystring-at-point ()
   "Returns the doxystring belonging to the function at point,
 even if point inside the function definition"
-  (cpp2clb/get-doxystring (cpp2clb/get-function-string-at-point)))
+  (cpp2clb/get-doxystring (cpp2clb/get-complete-definition-at-point)))
 
 (defun cpp2clb/get-documentation-string-at-point ()
   "Returns a documentation string for the function at point, 
@@ -144,17 +175,30 @@ even if point is in the doxystring."
    (cpp2clb/get-function-name
     (cpp2clb/get-function-definition-at-point))))
 
-(defun cpp2clb/find-current-class-name ()
-  "Return the name of the current class"
+(defun cpp2clb/find-current-environment-name
+    (environment-regexp api-regexp)
+  "Return the name of the current environment, environment
+being e.g. class, struct, enum,..."
   (save-excursion
     (re-search-backward
-     (concat cpp2clb/class-regexp cpp2clb/api-def-regexp))
+     (concat environment-regexp api-regexp))
     (re-search-forward
-     (concat cpp2clb/class-regexp cpp2clb/api-def-regexp))
+     (concat environment-regexp api-regexp))
     (set-mark (point))
     (forward-word)
     (cpp2clb/chomp
      (buffer-substring-no-properties (mark) (point)))))
+
+(defun cpp2clb/find-current-class-name ()
+  "Return the name of the current class"
+  (cpp2clb/find-current-environment-name
+   cpp2clb/class-regexp cpp2clb/api-def-regexp))
+
+(defun cpp2clb/find-current-enum-name ()
+  "Return the name of the current enum"
+  (cpp2clb/find-current-environment-name
+   cpp2clb/enum-regexp ""))
+
 
 (defun cpp2clb/get-current-class-name ()
   "Returns the current classname. If variable cpp2clb/current-class
@@ -224,7 +268,8 @@ into the previous window, at its point."
 	  (indent-region start (point) indcolumn)
 	  (newline)
 	  (newline)
-	  (c-indent-line-or-region))))))
+	  (c-indent-line-or-region))))
+    ))
 
 
 (defun cpp2clb/insert-clbind-static-def-previous-window ()
@@ -242,6 +287,148 @@ into the previous window, at its point."
 	  (newline)
 	  (c-indent-line-or-region))))))
 
+(defun cpp2clb/get-current-enum-mapper-string ()
+    "Returns a string that is the enum mapper for the current enum"
+  (concat
+   "*"
+   (upcase (cpp2clb/find-current-class-name)) "-"
+   (upcase (cpp2clb/find-current-enum-name))
+   "-ENUM-MAPPER*"))
+
+(defun cpp2clb/get-full-enum-designator ()
+  "Returns the full enum designator found at point"
+  (concat 
+   cpp2clb/current-namespace
+   "::"
+   (cpp2clb/find-current-class-name)
+   "::"
+   (cpp2clb/find-current-enum-name)))
+
+(defun cpp2clb/create-clbind-enum-def-from-point ()
+  "Returns the enum definition that suits the enum definition found at point"
+  (let ((return-string 
+	 (concat
+	  ",enum_<"
+	  (cpp2clb/get-full-enum-designator)
+	  ">(core::lisp_intern(\""
+	  cpp2clb/current-package
+	  "\", \""
+	  (cpp2clb/get-current-enum-mapper-string)
+	  "\"))\n[\n")))
+    (dolist
+	(enum-def
+	 (cpp2clb/get-lines-as-list
+	  (cpp2clb/get-brace-content
+	   (cpp2clb/get-non-doxystring
+	    (cpp2clb/get-complete-definition-at-point))
+	   "{" "}")))
+      (setq
+       return-string
+       (concat
+	return-string
+	"  value(\""
+	(cpp2clb/lispify-name (cpp2clb/find-current-class-name))
+	cpp2clb/enum-class-delimiter
+	(cpp2clb/lispify-name
+	 (cpp2clb/get-enum-member-from-line enum-def))
+	"\", "
+	(cpp2clb/get-full-enum-designator)
+	"::"
+	(cpp2clb/get-enum-member-from-line enum-def)
+	"),\n")))
+    (setq
+     return-string
+     (concat
+      (substring return-string 0 (- (length return-string) 2))
+      "\n]\n"))
+    return-string))
+
+(defun cpp2clb/create-clbind-enum-translators-from-point ()
+  "Create enum translators fitting for the enum at point."
+  (concat
+   "template <>
+  struct to_object<"
+   (cpp2clb/get-full-enum-designator)
+   ">
+  {
+    static core::T_sp convert("
+   (cpp2clb/get-full-enum-designator)
+   " v)
+    {
+      core::Symbol_sp converterSym = 
+        core::lisp_intern(\""
+   cpp2clb/current-package
+   "\",\""
+   (cpp2clb/get-current-enum-mapper-string)
+   "\");
+      core::SymbolToEnumConverter_sp converter = 
+        converterSym->symbolValue().as<core::SymbolToEnumConverter_O>();
+      return converter->symbolForEnum<"
+   (cpp2clb/get-full-enum-designator)
+   ">(v);
+    }
+  };
+
+  template <>
+  struct from_object<"
+   (cpp2clb/get-full-enum-designator)
+   ">
+  {
+    typedef "
+   (cpp2clb/get-full-enum-designator)
+   " DeclareType;
+    DeclareType _v;
+    from_object(core::T_sp object)
+    {
+      if (core::Symbol_sp sym = object.asOrNull<core::Symbol_O>()) 
+      {
+	if (sym.notnilp()) 
+        {
+	  core::Symbol_sp converterSym = core::lisp_intern(\""
+   cpp2clb/current-package
+   "\",\""
+   (cpp2clb/get-current-enum-mapper-string)
+   "\");
+	  core::SymbolToEnumConverter_sp converter = 
+            converterSym->symbolValue().as<core::SymbolToEnumConverter_O>();
+	  this->_v = converter->enumForSymbol<"
+   (cpp2clb/get-full-enum-designator)
+   ">(sym);
+	  return;
+	}
+      }
+      SIMPLE_ERROR(BF(\"Cannot convert object %s to "
+   (cpp2clb/get-full-enum-designator)
+   "\") % _rep_(object) );
+    }
+  };
+
+  "))
+
+(defun cpp2clb/insert-clbind-enum-translators-previous-window ()
+  "Insert enum translators fitting for the enum at point
+into the previous window."
+  (interactive)
+  (let ((clbind-def
+	 (cpp2clb/create-clbind-enum-translators-from-point)))
+    (cpp2clb/execute-function-previous-window
+     '(lambda ()
+	(insert clbind-def)))))
+
+(defun cpp2clb/insert-clbind-enum-def-previous-window ()
+  "Inserts the clbind def created from the current function 
+into the previous window, at its point."
+  (interactive)
+  (let ((clbind-def (cpp2clb/create-clbind-enum-def-from-point)))
+    (cpp2clb/execute-function-previous-window
+     '(lambda ()
+  	(let ((indcolumn (current-column))
+  	      (start (point)))
+  	  (insert clbind-def)
+  	  (indent-region start (point) indcolumn)
+  	  (newline)
+  	  (c-indent-line-or-region))))))
+
 
 (define-minor-mode cpp2clbind-mode
   "A minor mode for quickly creating clbind definitions
@@ -256,6 +443,14 @@ from cpp header file definitions."
       cpp2clbind-mode-map
       (kbd "s-n")
       'cpp2clb/insert-clbind-static-def-previous-window)
+    (define-key
+      cpp2clbind-mode-map
+      (kbd "s-m")
+      'cpp2clb/insert-clbind-enum-def-previous-window)
+    (define-key
+      cpp2clbind-mode-map
+      (kbd "s-v")
+      'cpp2clb/insert-clbind-enum-translators-previous-window)
     cpp2clbind-mode-map))
 
 (add-hook 'c++-mode-hook 'cpp2clbind-mode)
